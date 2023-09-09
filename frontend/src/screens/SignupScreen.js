6de96f823843ee4953c797bc0bc6f318ccb79cf9
Rishab1201 <rishabhjadhav1201@gmail.com>
2023-09-09 23:06:11 +0530

    Sign up

--- frontend/src/screens/SignupScreen.js
+++ frontend/src/screens/SignupScreen.js
@@ -48,6 +48,17 @@ export default function SignupScreen() {
     }
   }, [navigate, redirect, userInfo]);
 
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const [isConfirmPassword, setisConfirmPassword] = useState(false);
+
+  function togglePasswordVisibility() {
+    setIsPasswordVisible((prevState) => !prevState);
+  }
+
+  const toggleConfirmPassword = () => {
+    setisConfirmPassword((prevState) => !prevState);
+  }
+
   return (
     // <Container className="small-container">
     //   <Helmet>
@@ -520,7 +531,6 @@ export default function SignupScreen() {
                       />
                     </div>
                   </div>
-                  
                 </div>
                 <div className="flex -mx-3">
                   <div className="w-full px-3 mb-5">
@@ -536,7 +546,6 @@ export default function SignupScreen() {
                         className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
                         placeholder="johnsmith@example.com"
                         onChange={(e) => setEmail(e.target.value)}
-                        
                       />
                     </div>
                   </div>
@@ -550,13 +559,56 @@ export default function SignupScreen() {
                       <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
                         <i className="mdi mdi-lock-outline text-gray-400 text-lg" />
                       </div>
-                      <input
-                        type="password"
-                        className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
-                        placeholder="************"
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                      />
+                      <div className="w-full -ml-10 pl-10 rounded-lg border-2 outline-none relative border-gray-200 focus:border-indigo-500 bg-white">
+                        <input
+                          type={isPasswordVisible ? "text" : "password"}
+                          className="w-full pl-10 -ml-10 rounded-lg pr-3 py-2 rounded-r-lg outline-none "
+                          placeholder="************"
+                          onChange={(e) => setConfirmPassword(e.target.value)}
+                          required
+                        />
+                        <button
+                          onClick={togglePasswordVisibility}
+                          className="text-center"
+                        >
+                          {isPasswordVisible ? (
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth={1.5}
+                              stroke="currentColor"
+                              className="w-5 h-5 absolute right-2 top-3"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                              />
+                            </svg>
+                          ) : (
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth={1.5}
+                              stroke="currentColor"
+                              className="w-5 h-5 absolute right-2 top-3"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                              />
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                              />
+                            </svg>
+                          )}
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -569,16 +621,60 @@ export default function SignupScreen() {
                       <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
                         <i className="mdi mdi-lock-outline text-gray-400 text-lg" />
                       </div>
-                      <input
-                        type="text"
-                        className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
-                        placeholder="************"
-                        onChange={(e) => setConfirmPassword(e.target.value)}
-                        required
-                      />
+                      <div className="w-full -ml-10 pl-10 rounded-lg border-2 outline-none relative border-gray-200 focus:border-indigo-500 bg-white">
+                        <input
+                          type={isConfirmPassword ? "text" : "password"}
+                          className="w-full pl-10 -ml-10 rounded-lg pr-3 py-2 rounded-r-lg outline-none "
+                          placeholder="************"
+                          onChange={(e) => setConfirmPassword(e.target.value)}
+                          required
+                        />
+                        <button
+                          onClick={toggleConfirmPassword}
+                          className="text-center"
+                        >
+                          {isConfirmPassword ? (
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth={1.5}
+                              stroke="currentColor"
+                              className="w-5 h-5 absolute right-2 top-3"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                              />
+                            </svg>
+                          ) : (
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth={1.5}
+                              stroke="currentColor"
+                              className="w-5 h-5 absolute right-2 top-3"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                              />
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                              />
+                            </svg>
+                          )}
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </div>
+                {/* w-5 h-5 absolute right-2 top-3 */}
                 <p class="text-sm font-light text-black dark:text-gray-700 pb-3">
                   Already have an account?{" "}
                   <Link to={`/signin?redirect=${redirect}`}>
